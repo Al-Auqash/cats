dd053168d625d4be7e6240e2b635e27487860f5e
Al-Auqash <ilhamtawakhal@gmail.com>
2023-10-09 15:27:11 +0700

    spring boot - #2 added crud graphql

--- springboot3/src/main/java/com/itash/cats/controller/SpeciesQueryResolver.java
+++ springboot3/src/main/java/com/itash/cats/controller/SpeciesQueryResolver.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,13 +32,37 @@ public class SpeciesQueryResolver {
 
     @QueryMapping(name = "findAllSpecies")
     public List<SpeciesModel> findAllSpecies() {
-        System.out.println("findAllSpecies method called");
-
-        return DUMMY_SPECIES;
+        return repository.findAll();
     }
 
     @QueryMapping
     public Optional<SpeciesModel> findSpeciesById(@Argument Long id) {
         return repository.findById(id);
+    }
+
+    @MutationMapping(name = "createSpecies")
+    public SpeciesModel createSpecies(@Argument String species, @Argument String speciesName) {
+        SpeciesModel speciesModel = new SpeciesModel();
+        speciesModel.setSpecies(species);
+        speciesModel.setSpeciesName(speciesName);
+        return repository.save(speciesModel);
+    }
+
+    @MutationMapping(name = "updateSpecies")
+    public SpeciesModel updateSpecies(@Argument Long id, @Argument String species, @Argument String speciesName) {
+        SpeciesModel speciesModel = repository.findById(id).orElseThrow(() -> new RuntimeException("Species not found"));
+        speciesModel.setSpecies(species);
+        speciesModel.setSpeciesName(speciesName);
+        return repository.save(speciesModel);
+    }
+
+    @MutationMapping(name = "deleteSpecies")
+    public boolean deleteSpecies(@Argument Long id) {
+        try {
+            repository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
